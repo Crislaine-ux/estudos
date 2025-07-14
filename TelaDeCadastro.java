532cbc2ac11b811c052b42cf547ae6145ec565d9
Crislaine Lopes <Crislainelopes090@gmail.com>
2025-07-14 18:28:37 -0300

    Add files via upload

---
+++ TelaDeCadastro.java
@@ -0,0 +1,24 @@
+public class TelaDeCadastro
+{
+    public static void main (String [] args)
+    {
+
+        //variaveis -> tipo: Textos (Strings)
+        //variaveis -> tipo: numeroa (int)
+        //variaveis -> tipo: numeros fracionados (double)
+        //variaveis -> tipo: booleanas (verdadeiro/falso)
+
+        //TIPOS DE DADOS
+
+        String login= "Óla"; //representação DE TEXTOS (ASCII)
+        int number1=10; //Repressentações de numeros inteiros
+        double percent =24.30; // Representação e números fracionados
+        boolean isDriver = true; // Representação bpleana (Etado Verdadeiro/Falso)
+
+        System.out.println(login);
+        System.out.println(number1);
+        System.out.println(percent);
+        System.out.println(isDriver);
+
+    }
+}
